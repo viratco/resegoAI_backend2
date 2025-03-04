ea6868e9efd3418ae70da0527bc8c0114a34896c
viratco <meenukumarpk@gmail.com>
2025-03-04 18:28:45 +0530

    backend2

--- src/routes/report.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { authenticateToken } from '../middleware/auth';
-import { generateReport } from '../controllers/reportController';
-
-const router = Router();
-
-router.post('/generate-report', authenticateToken, generateReport);
-
-export default router; 
